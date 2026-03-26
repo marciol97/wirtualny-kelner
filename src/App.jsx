a5d7156c9016486f92f8ef4834065f42575b228c
marciol97 <lewandowskimarcio12@gmail.com>
2026-03-26 15:25:23 +0100

    Start projektu: React + Vite + Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+        <h1 className="text-4xl font-bold text-blue-600">
+          Wirtualny Kelner - Aplikacja dla nowoczesnej gastronomii
+        </h1>
+      </div>
+  )
+}
+
+export default App
